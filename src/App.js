34778076d0820d6a13d71e2743a0d4d728d9e501
Luis Palomino Trevilla <luispalominot3@gmail.com>
2019-02-09 12:50:17 -0600

    Added scss support and simple form example

--- src/App.js
+++ src/App.js
@@ -1,8 +1,61 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+class SimpleForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ''
+    }
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({
+      text: e.target.value
+    })
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    this.props.createText(this.state.text);
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={(e) => this.onSubmit(e)}>
+          <label>Texto:</label>
+          <input
+            type="text"
+            name="title"
+            onChange={(e) => {this.onChange(e)}}
+            value={this.state.text}>
+          </input>
+          <button type="submit"> Submit</button>
+        </form>
+      </div>
+    )
+  }
+}
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: 'HOLA MUNDO!'
+    }
+    this.changeText = this.changeText.bind(this);
+  }
+
+  changeText(text) {
+    this.setState({
+      text
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,15 +64,11 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <p>
+          {this.state.text}
+        </p>
+        <SimpleForm createText={this.changeText}/>
       </div>
     );
   }
