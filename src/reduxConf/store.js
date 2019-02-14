4bf7175e7c4a910a521be97790bd4592257fabe2
Luis Palomino Trevilla <luispalominot3@gmail.com>
2019-02-13 22:46:14 -0600

    Check if redux dev tools is installed before adding it to the composer

--- src/reduxConf/store.js
+++ src/reduxConf/store.js
@@ -6,13 +6,21 @@ const initialState = {};
 
 const middleware = [thunk];
 
+let devTools = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : null
+
+let composer = null;
+if (devTools) {
+    composer = compose(
+        applyMiddleware(...middleware),
+        devTools
+    );
+} else {
+    composer = applyMiddleware(...middleware);
+}
 const store = createStore(
     rootReducer,
     initialState,
-    compose(
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
+    composer
 );
 
 export default store;
