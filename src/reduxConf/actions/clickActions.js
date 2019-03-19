5b779f870f6db28ee1cbf412155b963759a88c77
Jorge Pietra Santa <jorgeps14@Jorges-MacBook-Pro.local>
2019-03-19 13:15:00 -0600

    Added showHideRegister

--- src/reduxConf/actions/clickActions.js
+++ src/reduxConf/actions/clickActions.js
@@ -1,4 +1,4 @@
-import { SET_MENU_VALUE, SET_LOG_VALUE } from './types';
+import { SET_MENU_VALUE, SET_LOG_VALUE, SET_REG_VALUE } from './types';
 export const showHideMenu = item => dispatch => {
     dispatch({
         type: SET_MENU_VALUE,
@@ -8,6 +8,13 @@ export const showHideMenu = item => dispatch => {
 export const showHideLogIn = item => dispatch => {
     dispatch({
         type: SET_LOG_VALUE,
+        payload: item
+    });
+}
+
+export const showHideRegister = item => dispatch => {
+    dispatch({
+        type: SET_REG_VALUE,
         payload: item
     });
 }
