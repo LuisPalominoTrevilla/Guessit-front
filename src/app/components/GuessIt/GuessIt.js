ae6a3a2a411bbc8b12ab6e685f7f01de4d53603f
Luis Palomino Trevilla <luispalominot3@gmail.com>
2019-04-15 20:51:42 -0500

    Basic image display in guessit view

--- src/app/components/GuessIt/GuessIt.js
+++ src/app/components/GuessIt/GuessIt.js
@@ -1,6 +1,26 @@
 import React, { Component } from 'react';
 import http from 'services/http';
 
+import './guessit.scss';
+
+const Image = props => {
+    return (
+        <div className="col-12 col-lg-3 col-md-4 col-sm-6">
+            <div className="image-container">
+                <img src={ props.url } alt="Imágen" className="user-image" />
+            </div>
+        </div>
+    )
+}
+
+const RenderImages = ({ images }) => {
+    return images.map(image =>
+        (
+            <Image key={image.id} url={`${process.env.REACT_APP_IMAGES_BASE_URL}${image.url}`} age={image.age} />
+        )
+    );
+};
+
 class GuessIt extends Component {
     constructor(props) {
         super(props);
@@ -12,7 +32,9 @@ class GuessIt extends Component {
     componentWillMount() {
         http.get('/Image')
             .then(({ images }) => {
-                console.log(images);
+                this.setState({
+                    images
+                });
             })
             .catch(err => {
                 console.log(err);
@@ -23,7 +45,7 @@ class GuessIt extends Component {
         return (
             <div className="container-fluid pt-3">
                 <div className="row">
-                    
+                    <RenderImages images={this.state.images} />
                 </div>
             </div>
         )
