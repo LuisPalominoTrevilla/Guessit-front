0f7bdf2d279467ccc579234af93ca90d755fd52d
dafnesabrina1 <dafnesabrina1@gmail.com>
2019-05-05 21:49:33 -0500

    Stats

--- src/app/components/UserImages/Images.js
+++ src/app/components/UserImages/Images.js
@@ -2,22 +2,77 @@ import React, { Component, Fragment } from 'react';
 import ImageUpload from 'app/components/ImageUpload/ImageUpload';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { Pie } from 'react-chartjs-2';
 
 import './images.scss';
 
+const options = {
+    maintainAspectRatio: false,
+    responsive: false,
+    legend: {
+      position: 'bottom',
+      labels: {
+        boxWidth: 10,
+        fontColor: "white",
+      }
+    }
+  }
+
 function Image(props) {
+    const data = {
+        labels: [
+            'Correct',
+            'Incorrect'
+        ],
+        datasets: [{
+            data: [props.correctUnregister, props.quantityUnregister - props.correctUnregister],
+            backgroundColor: [
+            '#ffffff'
+            ],
+        }]
+    }
+    const data1 = {
+        labels: [
+            'Correct',
+            'Incorrect'
+        ],
+        datasets: [{
+            data: [props.correctRegister, props.quantityRegister - props.correctRegister],
+            backgroundColor: [
+            '#ffffff'
+            ],
+        }]
+    }
     return (
         <div className="col-12 col-lg-3 col-md-4 col-sm-6">
             <div className="image-container">
                 <img src={ props.imageURL } alt="Imágen de usuario" className="user-image" />
+                <div className="overlay-image"> 
+                    <Pie
+                        data={data}
+                        options={options}
+                    />
+                    <Pie
+                        data={data1}
+                        options={options}
+                    />
+                
+                </div>
             </div>
+
         </div>
     )
 }
 
 function RenderImages({ images }) {
     return images.map(image => (
-        <Image key={image.id} imageURL={`${process.env.REACT_APP_IMAGES_BASE_URL}${image.url}`} />
+        <Image key={image.id} 
+        imageURL={`${process.env.REACT_APP_IMAGES_BASE_URL}${image.url}`}
+        correctUnregister={image.unregisteredGuesses.correct}
+        quantityUnregister={image.unregisteredGuesses.quantity}
+        correctRegister={image.registeredGuesses.correct}
+        quantityRegister={image.registeredGuesses.quantity}/>
+
     ));
 }
 
